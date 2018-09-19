5096de9ff456f91e9e41027c3e635ef1d3f8e0ab
hwaymel <42789557+hwaymel@users.noreply.github.com>
2018-09-18 21:37:18 -0600

    Add files via upload

--- GradeCalculator_Project_1/GradeCalculator_Project_1/frmCalculateGrade.cs
+++ GradeCalculator_Project_1/GradeCalculator_Project_1/frmCalculateGrade.cs
@@ -16,5 +16,44 @@ namespace GradeCalculator_Project_1
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declaring variables for use in if statements
+            decimal numberGrade = Convert.ToDecimal(txtNumberGrade.Text);
+            string letterGrade = "";
+
+            //If statements to determine/set letter grade variable based on numeric value entered
+            if (numberGrade >= 88)
+            {
+                letterGrade = "A";
+            }
+            else if (numberGrade >= 80 && numberGrade <= 87)
+            {
+                letterGrade = "B";
+            }
+            else if (numberGrade >= 68 && numberGrade <=79)
+            {
+                letterGrade = "C";
+            }
+            else if (numberGrade >= 60 && numberGrade <= 67)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+
+            //Displaying letter grade in text box and resetting focus
+            txtLetterGrade.Text = letterGrade;
+            txtNumberGrade.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close form when clicking exit
+            this.Close();
+        }
     }
 }
